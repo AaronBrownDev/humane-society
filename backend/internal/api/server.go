9adb2aa98c96d131a9ca9b543d3f3c9d8a95d37c
AaronBrownDev <aaronb954@gmail.com>
2025-03-24 15:53:52 -0500

    add comments

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -13,12 +13,14 @@ import (
 	"time"
 )
 
+// Server represents the HTTP server and its dependencies
 type Server struct {
-	router     http.Handler
-	httpServer *http.Server
-	db         *sql.DB
+	router     http.Handler // HTTP server router/mux
+	httpServer *http.Server // HTTP server instance
+	db         *sql.DB      // database connection pool
 }
 
+// NewServer returns a new Server instance
 func NewServer(db *sql.DB) *Server {
 	router := NewRouter(db)
 
@@ -28,20 +30,25 @@ func NewServer(db *sql.DB) *Server {
 	}
 }
 
+// Run starts the HTTP server and waits for shutdown
+// returns error if shutdown encounters an error
 func (s *Server) Run() error {
+	// Defines the port number for the server from environment or uses default
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	// Defines the server params
 	s.httpServer = &http.Server{
-		Addr:         ":" + port,
-		Handler:      s.router,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
-		IdleTimeout:  60 * time.Second,
+		Addr:         ":" + port,       // Port address
+		Handler:      s.router,         // Router
+		ReadTimeout:  15 * time.Second, // Max duration for read requests
+		WriteTimeout: 15 * time.Second, // Max duration for write responses
+		IdleTimeout:  time.Minute,      // Max duration to wait for next request
 	}
 
+	// Starts server
 	go func() {
 		log.Printf("Starting server on port %s", port)
 		if err := s.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -49,14 +56,19 @@ func (s *Server) Run() error {
 		}
 	}()
 
+	// Channel that receives signal for shutdown
 	quit := make(chan os.Signal, 1)
+	// Adds interrupt signals (CTRL + C) or termination signals to channel
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// Waits until signal is received
 	<-quit
 	log.Println("Shutting down server...")
 
+	// Gives 15 seconds for connections to end
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	// Stops accepting new connections
 	if err := s.httpServer.Shutdown(ctx); err != nil {
 		return fmt.Errorf("server forced to shutdown: %v", err)
 	}
