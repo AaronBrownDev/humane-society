85bb6e53b0d353d8551a720256b6b51436130423
AaronBrownDev <aaronb954@gmail.com>
2025-03-24 14:15:49 -0500

    add main.go logic

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"github.com/AaronBrownDev/HumaneSociety/internal/api"
 	"github.com/AaronBrownDev/HumaneSociety/internal/database"
-	"github.com/AaronBrownDev/HumaneSociety/internal/repository"
 	"log"
 )
 
 func main() {
+	// Initialize database
 	if err := database.InitializeDB(); err != nil {
 		log.Panicf("Error initializing database: %v", err)
 	}
 	defer database.CloseDB()
 
-	dogRepo := repository.NewDogRepository(database.GetDB())
+	// Get database
+	db := database.GetDB()
+
+	// Start server
+	server := api.NewServer(db)
+	if err := server.Run(); err != nil {
+		log.Panicf("Error starting server: %v", err)
+	}
 }
