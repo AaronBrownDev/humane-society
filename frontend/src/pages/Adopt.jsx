f0691c16012cb87cca8480d657877db7f1f3faca
courtnaejames1 <cimanijames@gmail.com>
2025-03-30 22:53:28 -0500

    update frontend

---
+++ frontend/src/pages/Adopt.jsx
@@ -0,0 +1,11 @@
+import Section8 from "../components/Section8"
+
+export default function Adopt (){
+return (
+
+    <>
+        <div className="Adopt-form"> </div>
+        <Section8/>
+    </>
+)
+}
