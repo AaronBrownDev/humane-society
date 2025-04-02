ce70665201035eb8cc32f111c6b98bb6598eea7b
courtnaejames1 <cimanijames@gmail.com>
2025-04-01 19:13:15 -0500

    add frontend changes

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,15 @@
+
+
 export default function Nav(){
     return(
+        
         <header className= "header">
             <nav className = "nav-bar">
                 <img src = "./src/assets/Humane-Society-logo2.png" />
                 <ul className ="nav-list">
                     <li><a href=" ">Home</a></li>
                     <li><a href="">About</a></li>
-                    <li><a href="">Adopt</a></li>
+                    <li><a href="ADOPT.jsx">Adopt</a></li>
                     <li><a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=THJZGG93QJ3JS&ssrt=1742335653866">Donate</a></li>
                     <li><a href="">Other Ways to Support</a></li>
                     <li><a href="">Volunteer</a></li>
@@ -17,5 +20,6 @@ export default function Nav(){
             </nav>
 
         </header>
+        
     )
 }
---
+++ frontend/src/pages/Volunteer.jsx
@@ -0,0 +1,24 @@
+import Nav from "../components/Nav.jsx";
+import Section4 from "../components/Section4.jsx";
+
+
+export default function Volunteer(){
+    return (
+        <>
+            <Nav/>
+            <div className="Volunteer form">
+                    <form>
+                        <label for="fName">First Name</label>
+                        
+                    </form>
+    
+            </div>
+            <Section4/>
+    
+        </>
+        
+    )
+
+
+
+}
