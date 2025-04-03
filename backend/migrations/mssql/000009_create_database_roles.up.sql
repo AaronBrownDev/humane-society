43a90df3211c83e1f5a07c283d2d36811ea655f2
AaronBrownDev <aaronb954@gmail.com>
2025-04-03 14:43:04 -0500

    add database roles

---
+++ backend/migrations/mssql/000009_create_database_roles.up.sql
@@ -0,0 +1,16 @@
+USE HumaneSociety;
+GO
+
+-- Create database roles
+CREATE ROLE HumaneSociety_Admin;
+GO
+
+CREATE ROLE HumaneSociety_Staff;
+GO
+
+CREATE ROLE HumaneSociety_Volunteer;
+GO
+
+CREATE ROLE HumaneSociety_ReadOnly;
+GO
+
