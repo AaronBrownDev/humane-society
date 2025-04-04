feba2480ac0a1af0d6e6521db3b495b9aae0f50d
Courtnae James <cimanijames@gmail.com>
2025-04-04 15:26:16 -0500

    Update frontend

---
+++ frontend/src/pages/Support.jsx
@@ -0,0 +1,8 @@
+export default function Support() {
+    return (
+      <div className="support-page">
+        <h1>Other Ways to Support</h1>
+        <p>This page is under development.</p>
+      </div>
+    )
+}
