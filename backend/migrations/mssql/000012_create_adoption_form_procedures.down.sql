4c5635d9320ec9ebf474c8d62eaa0b4eb9d53146
AaronBrownDev <aaronb954@gmail.com>
2025-04-07 12:28:29 -0500

    add adoption form procedures

---
+++ backend/migrations/mssql/000012_create_adoption_form_procedures.down.sql
@@ -0,0 +1,12 @@
+USE HumaneSociety;
+GO
+
+-- Drop the stored procedures in reverse order
+DROP PROCEDURE IF EXISTS RejectAdoptionForm;
+GO
+
+DROP PROCEDURE IF EXISTS CompleteAdoptionForm;
+GO
+
+DROP PROCEDURE IF EXISTS ApproveAdoptionForm;
+GO
