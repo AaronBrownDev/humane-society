8283a3f0960b0c9b82f4820fb86bfaacbefe7b2c
AaronBrownDev <aaronb954@gmail.com>
2025-04-20 21:38:19 -0500

    fix mssql and go domain type inconsistency

--- backend/internal/domain/user_role.go
+++ backend/internal/domain/user_role.go
@@ -8,14 +8,14 @@ import (
 
 type UserRole struct {
 	UserID   uuid.UUID `json:"user_id"`
-	RoleID   uuid.UUID `json:"role_id"`
+	RoleID   int       `json:"role_id"`
 	AssignAt time.Time `json:"assign_at"`
 }
 
 type UserRoleRepository interface {
 	GetAll(ctx context.Context) ([]UserRole, error)
 	GetByUserID(ctx context.Context, id uuid.UUID) (*UserRole, error)
-	GetByRoleID(ctx context.Context, id uuid.UUID) (*UserRole, error)
+	GetByRoleID(ctx context.Context, id int) (*UserRole, error)
 	Create(ctx context.Context, userRole UserRole) (UserRole, error)
 	Update(ctx context.Context, userRole UserRole) (UserRole, error)
 	Delete(ctx context.Context, id uuid.UUID) error
